3142b692331203287cf18a37dce3556d9a60196c
pujasonia78 <pujasonia78@gmail.com>
2026-09-08 07:12:53 +0700

    perbaiki

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -3,31 +3,26 @@ import pg from 'pg';
 const { Pool } = pg;
 
 export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: 'localhost',
+  port: 5432,
+  database: 'kolam_ikan2',
+  user: 'postgres',
+  password: 'magang123',   // ← langsung ditulis
 });
 
 // Test koneksi PostgreSQL
 export const testConnection = async () => {
   try {
     const client = await pool.connect();
-
     console.log('✅ PostgreSQL berhasil terhubung');
-
     client.release();
-
     return true;
   } catch (error) {
     console.error('❌ PostgreSQL gagal terhubung:', error.message);
-
     return false;
   }
 };
 
-// Menangani error dari PostgreSQL
 pool.on('error', (err) => {
   console.error('❌ PostgreSQL error:', err.message);
 });
